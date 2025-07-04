7ded57a57b16c8e0b81c9673d5a402d12fe690b8
RodrigoOctavio <rmaiabotelho@gmail.com>
2025-07-04 17:32:48 -0300

    Iniciei a criação dos modelos

--- filme/models.py
+++ filme/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+LISTA_CATEGORIA = (
+    ()
+)
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='')
+    descricao = models.TextField()
+    categoraia = models.CharField(max_length=50)
+    visualizacoes = 
+    data_criacao = 
