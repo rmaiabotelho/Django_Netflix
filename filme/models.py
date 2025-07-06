d9504323509cfd54e467ae175096f9860598dc76
Rodrigo <rmaiabotelho>
2025-07-06 11:09:53 -0300

    Criação do modelo Filme

--- filme/models.py
+++ filme/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 LISTA_CATEGORIA = (
-    ()
+    ('ANALISE', 'Análise'),
+    ('PROGRAMAÇÃO', 'Programação'),
+    ('APRESENTAÇÃO', 'Apresentação'),
+    ('OUTROS', 'Outros')
 )
 class Filme(models.Model):
     titulo = models.CharField(max_length=100)
-    thumb = models.ImageField(upload_to='')
-    descricao = models.TextField()
-    categoraia = models.CharField(max_length=50)
-    visualizacoes = 
-    data_criacao = 
+    thumb = models.ImageField(upload_to='thumb_filmes/')
+    descricao = models.TextField(max_length=1000)
+    categoria = models.CharField(max_length=20, choices=LISTA_CATEGORIA)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    
